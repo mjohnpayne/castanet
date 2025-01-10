84d30762d98d9b4761343ac77bae3e09c5747916
Rich Mayne <mayne941@gmail.com>
2025-01-10 11:31:23 +0000

    test: bowtie added to test suite

--- test/test_map_reads_to_ref.py
+++ test/test_map_reads_to_ref.py
@@ -40,6 +40,34 @@ def init_run_map(do_preprocessing, fake_files=False):
     shutil.rmtree(fstem)
 
 
+def init_run_map_bowtie2(do_preprocessing, fake_files=False):
+    '''Init'''
+    p = get_default_args()
+    p["Mapper"] = "bowtie2"
+    fstem = f"{p['SaveDir']}/{p['ExpName']}/"
+    os.mkdir(fstem)
+    if do_preprocessing:
+        '''Run preprocessing'''
+        init_preprocessing(p)
+    if fake_files:
+        p['ExpDir'] = get_random_str()
+    '''Call script to test'''
+    if fake_files:
+        '''Fake input files should raise stoperr'''
+        with pytest.raises(SystemError):
+            run_map(p)
+    else:
+        '''Real files should lead to a real BAM file output'''
+        run_map(p)
+        assert os.stat(
+            f"{p['SaveDir']}/{p['ExpName']}/{p['ExpName']}.bam").st_size > 2
+        if do_preprocessing:
+            '''Check deleted the temp files if did preprocessing'''
+            assert not os.path.exists(
+                f"rm {p['SaveDir']}/{p['ExpName']}/{p['ExpName']}_1_clean.fastq")
+    shutil.rmtree(fstem)
+
+
 def test_map_no_preprocessing():
     '''Test without preprocessing, i.e. map test data files'''
     init_run_map(do_preprocessing=False)
